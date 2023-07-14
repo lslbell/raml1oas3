10791d349cbb9ee4010b358960144e9ff15ec563
lslbell <lewissamuelbell@outlook.com>
2023-07-14 13:30:55 +0100

    javaa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+            System.out.println("Please provide a file path.");
+            return;
+        }
+
+        String filePath = args[0];
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(line);
+                bw.newLine();
+            }
+
+            System.out.println("File written successfully.");
+
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
     }
 }
